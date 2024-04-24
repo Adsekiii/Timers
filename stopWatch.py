aac89d314462acfd9b960972bed67062b52d6b34
Adsekiii <adrianzurawski@proton.me>
2024-04-24 21:55:27 +0200

    finished stopwatch.py for the time being

--- stopWatch.py
+++ stopWatch.py
@@ -4,14 +4,18 @@ import keyboard
 
 def stopwatch():
     ms = 0
+    timedTime : list = []
     print("Start by clicking the button (key) <F> ") 
     print("Time by clicking the button (key) <T> ")
     print("Stop by clicking the button (key) <S> ")
     if keyboard.read_key() == "f":
         while True:
             ms = startStopwatch(ms)
+            showTimed(ms, timedTime)
             if keyboard.is_pressed("s"):
                 break
+            elif keyboard.is_pressed("t"):
+                timeStopwatch(timedTime,ms)
             else:
                 pass
             
@@ -21,10 +25,16 @@ def startStopwatch(ms):
     time.sleep(1/20)
     clearTerminal.clearTerminal()
     ms += 5
-    print(int(ms/100), " : ", ms%100)
     return ms
     
+def timeStopwatch(timedTime, currTime) -> list:
+    timedTime.append(str(int(currTime/100)) + " : " + str(currTime%100))
+    return timedTime
+
+def showTimed(currTime, timedTime):
+    print(int(currTime/100), " : ", currTime%100)
+    i = 1
+    for x in timedTime:
+        print(i, "| ",x)
+        i+=1
         
-    
-def timeStopwatch():
-    pass
