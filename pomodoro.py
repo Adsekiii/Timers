6cb97e9747c9d9aca38032deb06513c66554c201
Adsekiii <adrianzurawski@proton.me>
2024-04-27 17:35:59 +0200

    refactored pomodoro.py, code is now much more readable and made simpler

--- pomodoro.py
+++ pomodoro.py
@@ -1,131 +1,94 @@
 import time
 import clearTerminal
-import keyboard
 
-# TODO global variables are not good in python, code need a rewrite to account for that
+# TODO Add gui and sound effects 
+# TODO Add invalid time input handling
+# TODO Add 'stop' button so that you can stop anytime
+# TODO after stop add display amount of cycles that user lasted as well as overall time worked and rested
 
-workPhase : int = 30
-shortBreak : int = 5
-longBreak : int = 15
-workPhaseNumber : int = 0
-isItWorkingTime = True
-
-def pomodoro() -> None :
-    global workPhaseNumber
-    clearTerminal.clearTerminal()
-    print("---POMODORO TIMER---")
-    print("Start Working phase! -> <F>")
-    print("Access options! -> <O>")
-    match keyboard.read_key():     #after pressing F start pomodoro
-        case 'f':
-            # TODO this pice of code need a desperate rewrite with less if statements
-            sec : int = workPhase * 60
-            while True:
-                if isItWorkingTime == True:
-                    if sec == 0:
-                        sec = workPhase * 60
-                    else:
-                        sec = startPomodoro(sec)
-                else:
-                    if sec == 0:
-                        workPhaseNumber += 1
-                        if workPhaseNumber % 3==0:
-                            sec = longBreak * 60
-                        else:
-                            sec = shortBreak * 60
-                    elif workPhaseNumber % 3==0:
-                        sec = startLongBreak(sec)
-                    else:
-                        sec = startShortBreak(sec)
-        case "o":
-            options()
+def pomodoroSetup() -> None :
+    workingTime : str = "10:10"
+    shortBreakTime : str = "0:02"
+    longBreakTime : str = "0:10"
+    working : bool = True
+    cycles : int = 0
+    
+    while True:
+        print("####POMODORO####")
+        print("1. Start Pomodoro")
+        print("2. Change Time")
+        print("3. Back")
+        choice : str = input()
+    
+        match choice:
+            case '1':
+                clearTerminal.clearTerminal()
+                pomodoro(workingTime, shortBreakTime, longBreakTime, working, cycles)
+                clearTerminal.clearTerminal()
+            case '2':
+                clearTerminal.clearTerminal()
+                workingTime, shortBreakTime, longBreakTime = timeSettings(workingTime, shortBreakTime, longBreakTime)
+                clearTerminal.clearTerminal()
+            case '3':
+                break
+            case _:
+                clearTerminal.clearTerminal()
+                print("invalid option try again!")
+        
+def timeSettings(workingTime,shortBreakTime,longBreakTime) -> tuple[str,str,str] :
+    print("Which time do you want to change?")
+    print("1. Work time")
+    print("2. Short break")
+    print("3. Long break")
+    choice : str = input()
+    match choice:
+        case '1':
+            clearTerminal.clearTerminal()
+            workingTime = timeChange(workingTime)
+        case '2':
+            clearTerminal.clearTerminal()
+            shortBreakTime = timeChange(shortBreakTime)
+        case '3':
+            clearTerminal.clearTerminal()
+            longBreakTime = timeChange(longBreakTime)
         case _:
-            print("INVALID VALUE! Try again!")
-            time.sleep(1)
-            pomodoro()
+            print("invalid option try again!")
+            timeSettings()
+    return workingTime, shortBreakTime, longBreakTime
 
-def startPomodoro(sec) -> int : #print current time, check if seconds are equal to 0 then choose which break to start
-    global isItWorkingTime
-    clearTerminal.clearTerminal()
-    print(int(sec/60), ":", sec%60)
-    sec -= 1
-    time.sleep(1)
-    if sec == 0:
-        clearTerminal.clearTerminal()
-        print(int(sec/60), ":", sec%60)
-        print("Work time is over click any key to proceed to break!")
-        isItWorkingTime = False
-        keyboard.read_key()
-    return sec
+def pomodoro(workingTime : str, shortBreakTime : str, longBreakTime : str, working : bool , cycles : int) -> None:
+    while True:
+        # TODO write a function for time assignment for 'seconds' variable
+        if cycles % 3 is 0 and working is False:
+            seconds : int = int(longBreakTime[:longBreakTime.find(":")])*60 + int(longBreakTime[longBreakTime.find(":")+1:])
+            print("Long break time!")
+            time.sleep(2)
+            countdown(seconds) 
+            working = not working 
+        elif cycles % 3 is not 0 and working is False:
+            seconds : int  = int(shortBreakTime[:shortBreakTime.find(":")])*60 + int(shortBreakTime[shortBreakTime.find(":")+1:])
+            print("Short break time!")
+            time.sleep(2)
+            countdown(seconds) 
+            working = not working 
+        else:
+            seconds : int  = int(workingTime[:workingTime.find(":")])*60 + int(workingTime[workingTime.find(":")+1:])
+            print("Working time!")
+            time.sleep(2)
+            countdown(seconds)
+            cycles += 1
+            working = not working 
 
-# TODO short and long break are identical so they should be refactored into singular function eventually
-def startShortBreak(sec) -> int :
-    global isItWorkingTime
-    clearTerminal.clearTerminal()
-    print(int(sec/60), ":", sec%60)
-    sec -= 1
-    time.sleep(1)
-    if sec == 0:
-        clearTerminal.clearTerminal()
-        print(int(sec/60), ":", sec%60)
-        print("Short break time is over click any key to proceed to work!")
-        isItWorkingTime = True
-        keyboard.read_key()
-    return sec
+def timeChange(timeToChange : str) -> str :
+    print("Specify how long should it last in mm:ss format: ",end="")
+    ################ TODO add error handling ################
+    timeToChange : str = input()
+    return timeToChange
 
-def startLongBreak(sec) -> int :
-    global isItWorkingTime
-    clearTerminal.clearTerminal()
-    print(int(sec/60), ":", sec%60)
-    sec -= 1
-    time.sleep(1)
-    if sec == 0:
+def countdown(seconds) -> None:
+    for i in range(seconds+1):
         clearTerminal.clearTerminal()
-        print(int(sec/60), ":", sec%60)
-        print("Long break time is over click any key to proceed to work!")
-        isItWorkingTime = True
-        keyboard.read_key()
-    return sec
-# TODO cases are similar so maybe add a separate function for it?
-def options() -> None :
-    global workPhase
-    global longBreak
-    global shortBreak
-    clearTerminal.clearTerminal()
-    print("Exit -> <X>")
-    print("Customize working time -> <Q>")
-    print("Customize long break time -> <W>")
-    print("Customize short break time -> <E>")
-    match keyboard.read_key():
-        case "x":
-            clearTerminal.clearTerminal()
-            pomodoro()
-        case "q":
-            workPhase = changeTime()
-            clearTerminal.clearTerminal()
-            print("successfully changed working time to ", workPhase, " minutes")
-            time.sleep(1)
-            pomodoro()
-        case "w":
-            longBreak = changeTime()
-            clearTerminal.clearTerminal()
-            print("successfully changed long break time to ", longBreak, " minutes")
-            time.sleep(1)
-            pomodoro()
-        case "e":
-            
-            shortBreak = changeTime()
-            clearTerminal.clearTerminal()
-            print("successfully changed short break time to ", shortBreak, " minutes")
-            time.sleep(1)
-            pomodoro()
-        case _:
-            print("INVALID VALUE! Try again!")
-            time.sleep(1)
-            options()
-             
-def changeTime() -> int :
-    print("Please enter how long should it lasts in minutes: ",end="")
-    type = input()
-    type = int(type)
-    return type
+        print(f"{"0" if int(seconds/60) < 10 else ""}{int(seconds/60)} : {"0" if seconds%60 < 10 else ""}{int(seconds%60)}")
+        seconds -= 1
+        time.sleep(1) 
+ 
